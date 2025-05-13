741d07c214c4c155f07122856a6631b01a1b7d80
Zabullah <zabee_zh111@yahoo.com>
2025-05-13 10:33:05 +0200

    feat : add kata Opposite

---
+++ src/codekata/Opposite.java
@@ -0,0 +1,12 @@
+package codekata;
+
+public class Opposite {
+    public static int getOpposite(int number){
+        return - number;
+    }
+    public static void main(String[] args){
+        System.out.println(getOpposite(1));
+        System.out.println(getOpposite(14));
+        System.out.println(getOpposite(-34));
+    }
+}
