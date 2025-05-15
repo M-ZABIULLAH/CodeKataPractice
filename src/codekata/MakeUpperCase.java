f46e3622cb674d2903f004df3b534f71478eb38e
Zabullah <zabee_zh111@yahoo.com>
2025-05-15 10:41:14 +0200

    feat : add class of kata (Make Upper Case).

---
+++ src/codekata/MakeUpperCase.java
@@ -0,0 +1,12 @@
+package codekata;
+
+public class MakeUpperCase {
+    public static String makeUpperCase(String str) {
+        return str.toUpperCase();
+        }
+    public static void main(String[] args){
+        String result = "hello ".toUpperCase();
+        System.out.println(result);
+    }
+
+}
